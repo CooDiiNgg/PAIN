f6328fedd92e442de4495cc57b2e42f9f78c20ce
Николай Вълканов <93319862+CooDiiNgg@users.noreply.github.com>
2024-04-18 09:40:14 +0300

    WORKS!!!!... sorta

--- Server/models.py
+++ Server/models.py
@@ -1,32 +1,84 @@
 from utils.utils import *
 from AI.AI_helper import *
+from dotenv import load_dotenv
 
 #for now only run it once
 
 ai = AI()
 reader = Reader()
 
-phishing = reader.get_phishing()
-normal = reader.get_normal()
 
-for i in range(10):
-    prompt = ai.create_prompt(phishing)
-    response = ai.get_response(prompt)
-    percentage = ai.get_persentage(response)
-    ai.clear_prompt()
-    print(percentage)
+callback = queue.Queue()
+end = queue.Queue()
+ai_response = queue.Queue()
+filters = {'number': 2, 'excluded_users': []}
+load_dotenv()
+email = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
 
-avrg = ai.get_avrg()
-print("AVRG:", avrg)
-ai.clear_all()
 
-for i in range(10):
-    prompt = ai.create_prompt(normal)
-    response = ai.get_response(prompt)
-    percentage = ai.get_persentage(response)
-    ai.clear_prompt()
-    print(percentage)
 
-avrg = ai.get_avrg()
-print("AVRG:", avrg)
-ai.clear_all()
+
+class Checker:
+    def __init__(self):
+        self.emails = []
+        self.email = None
+        self.text = ""
+        self.percentage = 0
+        self.avg = 0
+        self.prompt = ""
+    
+    
+    def get_email(self):
+        if len(self.emails) > 0:
+            self.email = self.emails.pop(0)
+            return self.email
+        return None
+    
+    def get_text(self):
+        self.text = str(self.email)
+        return self.text
+    
+    def get_percentage(self):
+        for i in range(10):
+            print(i)
+            print(self.text)
+            self.prompt = ai.create_prompt(self.text)
+            print(self.prompt)
+            response = ai.get_response(self.prompt)
+            print(response)
+            self.percentage = ai.get_percentage(response)
+            ai.clear_prompt()
+        self.avg = ai.get_avrg()
+        ai_response.put(self.avg)
+        return self.avg
+    
+    def clear_all(self):
+        ai.clear_all()
+        return True
+    
+    def run(self):
+        reader.read_multithreading(callback, filters, email, password, end)
+        while True:
+            if not end.empty():
+                break
+            if not callback.empty():
+                self.emails.append(callback.get())
+                if(self.get_email()):
+                    self.get_text()
+                    self.get_percentage()
+                    self.clear_all()
+            time.sleep(1)
+        return True
+    
+
+checker = Checker()
+try:
+    threading.Thread(target=checker.run).start()
+    while True:
+        if not ai_response.empty():
+            print(ai_response.get())
+except KeyboardInterrupt:
+    end.put(True)
+    print("Exiting")
+    exit(0)
--- Server/utils/utils.py
+++ Server/utils/utils.py
@@ -2,18 +2,36 @@
 # we need a phishing email and a normal email to test the model (It is not trained for that but i think it will work)
 
 import os
+import threading
+import time
+import queue
+from utils import email_reader
 
 class Reader:
     def __init__(self):
-        self.path = os.path.dirname(__file__)
-        self.path = self.path + "/phishing.txt"
-        self.phishing = open(self.path, 'r').read()
-        self.path = os.path.dirname(__file__)
-        self.path = self.path + "/normal.txt"
-        self.normal = open(self.path, 'r').read()
-
-    def get_phishing(self):
-        return self.phishing
-
-    def get_normal(self):
-        return self.normal
+        self.mail_obj = None
+    
+    def read_emails(self, filters, mail, password):
+        self.mail_obj = email_reader.EmailReader(mail, password)
+        self.mail_obj.login()
+        # filters = {'number': 2, 'excluded_users': []}
+        emails = self.mail_obj.get_emails(filters)
+        # mail.debug_print()
+        self.mail_obj.close()
+        return emails
+    
+    def multithreading(self, callback, filters, end, mail, password):
+        while True:
+            emails = self.read_emails(filters, mail, password)
+            for email in emails:
+                text = self.mail_obj.get_plain_text(email)
+                callback.put(text)
+            # print("Reading emails")
+            time.sleep(240)
+            if not end.empty():
+                break
+        return True
+    
+    def read_multithreading(self, callback, filters, mail, password, end):
+        threading.Thread(target=self.multithreading, args=(callback, filters, end, mail, password, )).start()
+        return True
